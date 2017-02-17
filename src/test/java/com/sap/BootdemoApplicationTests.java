6ed29a01e54045a66989e71c323633230f5d3d86
harcy <liangchen.shu@gmail.com>
2017-02-17 18:53:28 +0800

    check ehcache

--- src/test/java/com/sap/BootdemoApplicationTests.java
+++ src/test/java/com/sap/BootdemoApplicationTests.java
@@ -2,12 +2,19 @@ package com.sap;
 
 import com.sap.entity.User;
 import com.sap.mapper.UserMapper;
+import org.apache.ibatis.scripting.xmltags.ForEachSqlNode;
 import org.springframework.cache.CacheManager;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
@@ -23,7 +30,7 @@ public class BootdemoApplicationTests {
     @Test
     public void test() {
 
-        /*
+        /**
         * .yml配置打开执行的sql语句,当访问数据库拿数据时会显示sql
         * 而从缓存中直接get数据不会显示执行了sql
         * 如何确定cacheManager对象的实例, spring的CacheManager
@@ -34,6 +41,28 @@ public class BootdemoApplicationTests {
 
         User u2 = userMapper.findByUserName("lucy");
         System.out.println(u2.getUserAge());
+
+        /*net.sf.ehcache.CacheManager cacheManager1 =
+                net.sf.ehcache.CacheManager.newInstance("/ehcache.xml");
+        String[] strs = cacheManager1.getCacheNames();//classpath:Conf
+        for (int i=0; i<strs.length; i++) {
+            System.out.println("--hello--"+strs[i]);
+        }*/
+        /**
+         * asList(Object o)参数支持基本类型之后的坑
+         * 传入基本类型的数组后,所有的元素将全部转化为list的第一个元素
+         * list的size永远都是1
+         * */
+        Collection<String> strings = cacheManager.getCacheNames();
+        char[] chars = new char[]{'h','i','i','l'};
+        List list = Arrays.asList(chars);
+        System.out.println(list.size());
+        for (String str:strings
+                ) {
+            System.out.println("----------");
+            System.out.println(str);
+            System.out.println("----------");
+        }
 
         /*u1.setUserAge(22);
         userMapper.save(u1);*/
